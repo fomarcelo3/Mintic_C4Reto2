76be4307e68f726e271d569aab7599580a92005c
Fredy Marcelo <fredy.marcelo.mt@correo.usa.edu.co>
2021-12-04 13:45:47 -0500

    Se modifica clase main para borrar entidades

--- src/main/java/com/mintic/tienda/TiendaApplication.java
+++ src/main/java/com/mintic/tienda/TiendaApplication.java
@@ -1,13 +1,30 @@
 package com.mintic.tienda;
 
+import com.mintic.tienda.repository.crud.ClotheCrudRepository;
+import com.mintic.tienda.repository.crud.UserCrudRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
+@Component
 @SpringBootApplication
-public class TiendaApplication {
+public class TiendaApplication implements CommandLineRunner {
+
+	@Autowired
+	private ClotheCrudRepository clotheCrudRepository;
+	@Autowired
+	private UserCrudRepository userCrudRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TiendaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception{
+		clotheCrudRepository.deleteAll();
+		userCrudRepository.deleteAll();
 	}
 
 }
