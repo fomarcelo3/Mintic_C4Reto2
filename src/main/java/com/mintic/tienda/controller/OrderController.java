e67f4f31349d0652cc36146e3ebcd3d254e98ea3
Fredy Marcelo <fredy.marcelo.mt@correo.usa.edu.co>
2021-12-20 00:14:18 -0500

    Correccion 4 - 5

--- src/main/java/com/mintic/tienda/controller/OrderController.java
+++ src/main/java/com/mintic/tienda/controller/OrderController.java
@@ -58,7 +58,7 @@ public class OrderController {
     }
 
     //Metodo para ortener ordenes por estado y asesor
-    @GetMapping("/order/{state}/{id}")
+    @GetMapping("/{state}/{id}")
     public List<Order> ordersSalesManByStateAndId(  @PathVariable("state") String state,@PathVariable("id") Integer id) {
         return orderService.ordersSalesManByStateAndId(state,id);
     }
